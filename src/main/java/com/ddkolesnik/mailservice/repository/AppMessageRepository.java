9ba7f13fd1cd5e4f0aad07d1002750d4a0a9a09f
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-05-05 14:08:12 +0500

    Добавлен метод поиска сообщений, которые содержали ошибки во времня предыдущей отправки

--- src/main/java/com/ddkolesnik/mailservice/repository/AppMessageRepository.java
+++ src/main/java/com/ddkolesnik/mailservice/repository/AppMessageRepository.java
@@ -4,10 +4,15 @@ import com.ddkolesnik.mailservice.model.AppMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Alexandr Stegnin
  */
 
 @Repository
 public interface AppMessageRepository extends JpaRepository<AppMessage, Long> {
+
+    List<AppMessage> findByErrorNotNull();
+
 }
