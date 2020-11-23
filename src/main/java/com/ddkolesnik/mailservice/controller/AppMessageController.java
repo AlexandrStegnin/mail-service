f0897cbc5e944d1de50a6981016c2ece5804a087
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-23 22:23:21 +0500

    Логируем отправку сообщения.

--- src/main/java/com/ddkolesnik/mailservice/controller/AppMessageController.java
+++ src/main/java/com/ddkolesnik/mailservice/controller/AppMessageController.java
@@ -6,6 +6,7 @@ import com.ddkolesnik.mailservice.service.AppMessageService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import javax.validation.Valid;
  * @author Alexandr Stegnin
  */
 
+@Slf4j
 @SuppressWarnings("unused")
 @Validated
 @RestController
@@ -30,6 +32,7 @@ public class AppMessageController {
     public void sendWelcomeMessage(@PathVariable(name = "token") @ValidToken String token,
                                    @RequestBody @Valid AppUserDTO userDTO) {
         appMessageService.sendWelcomeMessage(userDTO);
+        log.info("Сообщение успешно отправлено пользователю [{}]", userDTO);
     }
 
 }
